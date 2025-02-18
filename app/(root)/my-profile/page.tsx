dea2cf90f9de6e3500c035b7ff0022bbc143da8d
saigopal-dev <saigopal.mepstra@gmail.com>
2025-02-18 11:39:25 +0530

    updated my-profile

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -22,7 +22,7 @@ const Page = async () => {
             <form
                 action={handleSignOut} // Calling server-side action for sign-out
                 method="POST"
-                className="mb-10 ml-[10px] md:ml-[800px] lg:ml-[1300px]"
+                className="mb-10 ml-[10px] md:ml-[550px] lg:ml-[800px]"
             >
                 <Button className="bg-blue-600 text-white hover:bg-blue-700 transition-all duration-300 rounded-lg px-6 py-2 shadow-lg transform hover:scale-105">
                     Logout
@@ -30,27 +30,27 @@ const Page = async () => {
             </form>
 
             <div className="flex justify-center items-center min-h-screen md:bg-gradient-to-r md:from-blue-500 md:to-purple-500 py-12">
-                <div className="bg-white rounded-xl shadow-2xl w-full max-w-lg p-8 space-y-6 ring-1 ring-gray-200">
-                    <h1 className="text-3xl font-extrabold text-center text-gray-800">My Profile</h1>
+                <div className="bg-dark-400 rounded-xl shadow-2xl w-full max-w-lg p-8 space-y-6">
+                    <h1 className="text-3xl font-extrabold text-center text-white">My Profile</h1>
                     <div className="text-gray-700">
                         {/* Render user details if available */}
                         {userDetails ? (
                             <div className="space-y-6">
                                 <div className="flex justify-between items-center p-4 rounded-lg bg-gray-50 shadow-md">
-                                    <span className="font-semibold text-gray-600">Full Name:</span>
-                                    <span className="text-gray-800">{userDetails.fullName}</span>
+                                    <span className="text-xs sm:text-sm md:text-md font-semibold text-black">Full Name:</span>
+                                    <span className="text-xs sm:text-sm md:text-md text-black">{userDetails.fullName}</span>
                                 </div>
                                 <div className="flex justify-between items-center p-4 rounded-lg bg-gray-50 shadow-md">
-                                    <span className="font-semibold text-gray-600">Email:</span>
-                                    <span className="text-gray-800">{userDetails.email}</span>
+                                    <span className="text-xs sm:text-sm md:text-md font-semibold text-black">Email:</span>
+                                    <span className="text-xs sm:text-sm md:text-md text-black">{userDetails.email}</span>
                                 </div>
                                 <div className="flex justify-between items-center p-4 rounded-lg bg-gray-50 shadow-md">
-                                    <span className="font-semibold text-gray-600">Last Activity:</span>
-                                    <span className="text-gray-800">{userDetails.lastActivityDate}</span>
+                                    <span className="text-xs sm:text-sm md:text-md font-semibold text-black">Last Activity:</span>
+                                    <span className="text-xs sm:text-sm md:text-md text-black">{userDetails.lastActivityDate}</span>
                                 </div>
                                 <div className="flex justify-between items-center p-4 rounded-lg bg-gray-50 shadow-md">
-                                    <span className="font-semibold text-gray-600">Created At:</span>
-                                    <span className="text-gray-800">{formattedDate}</span>
+                                    <span className="text-xs sm:text-sm md:text-md font-semibold text-black">Created At:</span>
+                                    <span className="text-xs sm:text-sm md:text-md text-black">{formattedDate}</span>
                                 </div>
                             </div>
                         ) : (
